d9c8f388f3eba9cf651463788bd409e6e76a400d
Dennis <ratenniel@gmail.com>
2018-10-10 20:06:55 +0200

    intentando hacer funcionar nodemailer 🤬

--- server/mail/sendMail.js
+++ server/mail/sendMail.js
@@ -1,27 +1,26 @@
-const nodemailer = require('nodemailer');
 require('dotenv').config();
+const nodemailer = require('nodemailer');
 
 if(!process.env.GMAIL_USER || ! process.env.GMAIL_PASSW ){
   throw new Error("You have to configure mail credentials in .private.env file.");
 }
 
-let transporter = nodemailer.createTransport({
-  service: 'Gmail',
+let transport = {
+  host: 'smtp.gmail.com',
   auth: {
-    user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_PASSW 
+    user: creds.USER,
+    pass: creds.PASS
+  }
+}
+
+var transporter = nodemailer.createTransport(transport)
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Server is ready to take messages');
   }
 });
 
-const sendMail = (to, subject, message)=>{
-  return transporter.sendMail({
-    to, 
-    subject, 
-    html: `<b>${message}</b>`
-  })
-  .then(info => console.log(info))
-  .catch(error => console.log(error))
-}
-
-
-module.exports = sendMail;
+export default sendMail;
